1867e3f61bd35b534b501f9645202a73abe95237
ruiy <2997215859@qq.com>
2020-03-25 01:50:19 +0800

    主要的一个大更改是写了一个neon版本的矩阵和向量之间的欧式距离
    main.cpp 热身赛 2020-03-25 01:47:17 0.1918 70% PASSED. 无

--- KM/main.cpp
+++ KM/main.cpp
@@ -27,6 +27,10 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define CacheLineSize 64
+#define IB CacheLineSize / sizeof(float)
+//#define IB 2
+
 using namespace std;
 
 int features_num = 1000;
@@ -37,10 +41,92 @@ static int buf_len_train_file;
 
 static char * buf;
 
-vector<int> train_labels;
+vector<int> train_labels(read_line_num);
 
-vector<vector<float>> train_features;
+vector<vector<float>> train_features(read_line_num, vector<float>(features_num, 0.0));
 
+vector<vector<float>> test_features(20000, vector<float>(1000));
+
+
+float dot (const vector<float> & vec1, const vector<float> & vec2) {
+    float sum = 0.0;
+#ifdef NO_NEON
+
+    for (int i = 0; i < vec1.size(); i++) {
+        sum += vec1[i] * vec2[i];
+    }
+#else
+    int len = vec1.size();
+    const float * p_vec1 = &vec1[0];
+    const float * p_vec2 = &vec2[0];
+
+
+    float32x4_t sum_vec = vdupq_n_f32(0),left_vec,right_vec;
+    for(int i = 0; i < len; i += 4)
+    {
+        left_vec = vld1q_f32(p_vec1 + i);
+        right_vec = vld1q_f32(p_vec2 + i);
+        sum_vec = vmlaq_f32(sum_vec, left_vec, right_vec);
+    }
+
+    float32x2_t r = vadd_f32(vget_high_f32(sum_vec), vget_low_f32(sum_vec));
+    sum += vget_lane_f32(vpadd_f32(r,r),0);
+
+#endif
+    return sum;
+}
+
+void EuclideanDist (const vector<vector<float>> & a, const vector<float> & b, vector<float> & c) { // 不开根号
+    int M = a.size();
+    int N = a[0].size();
+
+#ifdef NO_NEON
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            c[i] += (a[i][j] - b[j]) * (a[i][j] - b[j]);
+        }
+    }
+
+#else
+
+    int i = 0;
+    for (i = 0; i + IB < M; i += IB) {
+        float32x4_t temp[IB];
+        for (int ii = 0; ii < IB; ii++) {
+            temp[ii] = vdupq_n_f32(0.0f);
+        }
+
+
+        for (int k = 0; k < N; k += 4) {
+
+            float32x4_t v_b = vld1q_f32(&b[k]);
+
+            float32x4_t v_a[IB];
+            for (int ii = 0; ii < IB; ii++) {
+                v_a[ii] = vld1q_f32(&a[i + ii][k]);
+                v_a[ii] = vsubq_f32(v_a[ii], v_b);
+            }
+
+            for (int ii = 0; ii < IB; ii++) {
+                temp[ii] = vmlaq_f32(temp[ii], v_a[ii], v_a[ii]);
+            }
+
+        }
+
+
+        for (int ii = 0; ii < IB; ii++) {
+            float32x4_t temp_c = temp[ii];
+            c[i + ii] = (temp_c[0] + temp_c[1]) + (temp_c[2] + temp_c[3]);
+        }
+
+
+    }
+
+
+#endif
+
+}
 
 inline int getc() {
     if (buf_len_train_file < 0) return EOF;
@@ -52,8 +138,6 @@ inline float GetOneFloatData() {
 
     int f = 1;
     char ch = getc();
-
-    if (ch == char(EOF)) return -2;
 
     if (ch == '-') {f = -1; ch = getc();}
 
@@ -145,31 +229,19 @@ void LoadTrainData (const string & filename) {
 
     int cnt = 0;
 
-
-    vector<float> feature(features_num, 0.0);
-
     while (cnt < read_line_num) {
 
         int f_cnt = 0;
-        int eof_flag = 0;
-        int neg_flag = 0;
+
         while (f_cnt < features_num) {
             float f = GetOneFloatData();
-
-            if (f == -2) {eof_flag = 1; break;}
-            feature[f_cnt++] = f;
+            train_features[cnt][f_cnt++] = f;
             getc();
         }
 
-        if (eof_flag) break;
-
-        int ftf =  getc() - '0';
+        train_labels[cnt] =  getc() - '0';
         getc(); // 获取回车键
 
-        if (neg_flag) continue;
-
-        train_features.emplace_back(feature);
-        train_labels.emplace_back(ftf);
 
         cnt++;
 
@@ -187,20 +259,24 @@ void LoadTrainData (const string & filename) {
 
 
 
-vector<vector<float>> LoadTestDataMultiThread (const string & filename) {
+void LoadTestDataMultiThread (const string & filename) {
+
+#ifdef TEST
+    clock_t start_time = clock();
+#endif
 
     int fd = open(filename.c_str(), O_RDONLY);
 
     char * buf1 = (char *) mmap(NULL, 120000000, PROT_READ, MAP_PRIVATE, fd, 0);
 
 
-    vector<vector<float>> data_set(20000, vector<float>(1000));
+
 
     int thread_num = 8;
     int cnt_per_thread = 20000 / thread_num;
     vector<thread> thread_vec;
     for (int i = 0; i < thread_num; i++) {
-        thread_vec.emplace_back(SplitTestFunc, ref(data_set), buf1 + cnt_per_thread * 6000 * i, i * cnt_per_thread, cnt_per_thread);
+        thread_vec.emplace_back(SplitTestFunc, ref(test_features), buf1 + cnt_per_thread * 6000 * i, i * cnt_per_thread, cnt_per_thread);
     }
 
     for (int i = 0; i < thread_vec.size(); i++) {
@@ -210,11 +286,11 @@ vector<vector<float>> LoadTestDataMultiThread (const string & filename) {
 
     close(fd);
 
-
-    return data_set;
-
+#ifdef TEST
+    clock_t end_time = clock();
+    printf("测试集读取耗时（s）: %f \n", (double) (end_time - start_time) / CLOCKS_PER_SEC);
+#endif
 }
-
 
 void StorePredict(const vector<char> & predict_vec, const string & predict_file)
 {
@@ -235,6 +311,78 @@ void StorePredict(const vector<char> & predict_vec, const string & predict_file)
     memcpy(p, pData, DATA_LEN);
 
 }
+
+void Predict (const string & predict_file) {
+#ifdef TEST
+    clock_t start_time = clock();
+#endif
+
+
+    vector<float> mean_features0(features_num, 0.0);
+    int cnt0 = 0;
+    vector<float> mean_features1(features_num, 0.0);
+    int cnt1 = 0;
+
+    for (int i = 0; i < train_features.size(); i++) {
+        if (train_labels[i]) {
+            for (int j = 0; j < features_num; j++) {
+                mean_features1[j] += train_features[i][j];
+            }
+            cnt1++;
+        } else {
+            for (int j = 0; j < features_num; j++) {
+                mean_features0[j] += train_features[i][j];
+            }
+            cnt0++;
+        }
+    }
+
+    for (int j = 0; j < features_num; j++) {
+        mean_features0[j] /= cnt0;
+        mean_features1[j] /= cnt1;
+    }
+
+
+#ifdef TEST
+    clock_t cal_end_time = clock();
+    printf("计算平均值阶段耗时（s）: %f \n", (double) (cal_end_time - start_time) / CLOCKS_PER_SEC);
+#endif
+
+    vector<char> pred_vec(test_features.size());
+
+    vector<float> norm0(test_features.size());
+    vector<float> norm1(test_features.size());
+    EuclideanDist(test_features, mean_features0, norm0);
+    EuclideanDist(test_features, mean_features1, norm1);
+
+    for (int i = 0; i < test_features.size(); i++) {
+        if (norm0[i] < norm1[i]) {
+            pred_vec[i] = '0';
+        } else {
+            pred_vec[i] = '1';
+        }
+    }
+
+
+//    for (int i = 0; i < test_features.size(); i++) {
+//        float norm0 = dot(test_features[i], test_features[i]);
+//        float norm1 = dot(test_features1[i], test_features1[i]);
+//        if (norm0 < norm1) {
+//            pred_vec[i] = '0';
+//        } else {
+//            pred_vec[i] = '1';
+//        }
+//    }
+
+#ifdef TEST
+    clock_t end_time = clock();
+    printf("预测阶段耗时（s）: %f \n", (double) (end_time - cal_end_time) / CLOCKS_PER_SEC);
+#endif
+    StorePredict(pred_vec, predict_file);
+
+}
+
+
 bool loadAnswerData(string awFile, vector<int> & awVec) {
     ifstream infile(awFile.c_str());
     if (!infile) {
@@ -314,53 +462,11 @@ int main (int argc, char *argv[]) {
 
     LoadTrainData(train_file);
 
-    vector<int> train_labels_cp = train_labels;
+    LoadTestDataMultiThread(test_file);
 
-    vector<vector<float>> train_features_cp = train_features;
+    Predict(predict_file);
 
-    vector<float> mean_features0(features_num, 0.0);
-    int cnt0 = 0;
-    vector<float> mean_features1(features_num, 0.0);
-    int cnt1 = 0;
 
-    for (int i = 0; i < train_features.size(); i++) {
-        if (train_labels[i]) {
-            for (int j = 0; j < features_num; j++) {
-                mean_features1[j] += train_features[i][j];
-            }
-            cnt1++;
-        } else {
-            for (int j = 0; j < features_num; j++) {
-                mean_features0[j] += train_features[i][j];
-            }
-            cnt0++;
-        }
-    }
-
-    for (int j = 0; j < features_num; j++) {
-        mean_features0[j] /= cnt0;
-        mean_features1[j] /= cnt1;
-    }
-
-    vector<vector<float>> test_data = LoadTestDataMultiThread(test_file);
-
-    vector<char> pred_vec(test_data.size());
-    for (int i = 0; i < test_data.size(); i++) {
-        float norm0 = 0.0;
-        float norm1 = 0.0;
-        for (int j = 0; j < features_num; j++) {
-            norm0 += pow(mean_features0[j] - test_data[i][j], 2);
-            norm1 += pow(mean_features1[j] - test_data[i][j], 2);
-        }
-
-        if (norm0 < norm1) {
-            pred_vec[i] = '0';
-        } else {
-            pred_vec[i] = '1';
-        }
-    }
-
-    StorePredict(pred_vec, predict_file);
 
 #ifdef TEST
     clock_t end_time = clock();
